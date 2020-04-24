f9ccf6280e8f98eabc6ad526d340b743ecf183aa
Joy-Anurag-Mohanty <josephmohanty8999@gmail.com>
2020-04-24 11:41:39 +0530

    Sentiment Analysis

---
+++ Falsetto/src/com/nlp/model/SentimentClassification.java
@@ -0,0 +1,40 @@
+package com.nlp.model;
+
+public class SentimentClassification 
+{
+	double negative;
+	double neutral;
+	double positive;
+	
+	public double getNegative() 
+	{
+		return negative;
+	}
+	
+	public void setNegative(double negative) 
+	{
+		this.negative = negative;
+	}
+	
+	public double getNeutral() 
+	{
+		return neutral;
+	}
+	
+	public void setNeutral(double neutral) 
+	{
+		this.neutral = neutral;
+	}
+	
+	public double getPositive() 
+	{
+		return positive;
+	}
+	
+	public void setPositive(double positive) 
+	{
+		this.positive = positive;
+	}
+	
+	
+}
